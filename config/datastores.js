a54e75931700b7829a50b35db7c8fa40bff62092
Yoann Bertuol <bertuol@iutrs.unistra.fr>
2020-12-18 14:15:02 +0100

    Add MySQL Database

--- config/datastores.js
+++ config/datastores.js
@@ -50,7 +50,8 @@ module.exports.datastores = {
     ***************************************************************************/
     // adapter: 'sails-mysql',
     // url: 'mysql://user:password@host:port/database',
-
+    adapter: 'sails-mysql',
+    url: process.env.MYSQL_DATABASE_URL
   },
 
 
