b3aa056bfbc03c8de2b8daea87ca93db04a2acfe
Yoann Bertuol <bertuol@iutrs.unistra.fr>
2021-01-08 16:51:54 +0100

    Image implementation

--- api/models/Image.js
+++ api/models/Image.js
@@ -1,14 +1,11 @@
 /**
- * User.js
+ * Image.js
  *
  * @description :: A model definition represents a database table/collection.
  * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
  */
 
-const bcrypt = require('bcrypt');
-
 module.exports = {
-  migrate: "safe",
 
   attributes: {
 
@@ -16,14 +13,18 @@ module.exports = {
     //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
     //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
 
-    username: {
+    path: {
       type: 'string',
       required: true
     },
 
-    password: {
+    name: {
       type: 'string',
       required: true
+    },
+
+    uploader: {
+      model: 'user'
     },
 
     //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
@@ -35,31 +36,8 @@ module.exports = {
     //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
     //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
 
-    images: {
-      collection : 'image',
-      via : 'uploader'
-    }
-  },
-  customToJSON: function() {
-    return _.omit(this, ['password']);
-  },
-  beforeCreate: function(user, cb){
-    bcrypt.genSalt(10, (err, salt) => {
-      if(err) { return cb(err); }
-      bcrypt.hash(user.password, salt, (err, hash) => {
-        if(err) { return cb(err); }
-        user.password = hash;
-        return cb();
-      });
-    });
+
   },
 
-  signup: function(inputs, cb) {
-    User.create({
-      username: inputs.username,
-      password: inputs.password
-    })
-    .exec(cb);
-  }
 };
 
