cfbc4bcc2a71d14e59789a7b89a517530f90961c
Yoann Bertuol <bertuol@iutrs.unistra.fr>
2021-01-15 14:13:59 +0100

    Adding Image model

--- api/models/User.js
+++ api/models/User.js
@@ -36,8 +36,8 @@ module.exports = {
     //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
 
     images: {
-      collection : 'image',
-      via : 'uploader'
+      collection: 'image',
+      via: 'uploader'
     }
   },
   customToJSON: function() {
--- migrations/20210108150404_create_image_table.js
+++ migrations/20210108150404_create_image_table.js
@@ -9,7 +9,6 @@ exports.up = function(knex) {
 	});
    };
 	
-   exports.down = function(knex) {
-	return knex.schema.dropTable('image');
-   };
-   
+exports.down = function(knex) {
+return knex.schema.dropTable('image');
+};
